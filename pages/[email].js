1db8cd2d0ac9b2a20a1ac0ec0ba657d23d9f51e3
asfink <ali@finkelstein.us>
2020-09-20 21:47:43 -0700

    stylizied and added puppers

--- pages/[email].js
+++ pages/[email].js
@@ -3,6 +3,7 @@ import Router from 'next/router'
 import fetch from 'isomorphic-unfetch'
 import {parseCookies, destroyCookie} from 'nookies'
 import { useEffect } from 'react'
+import { PRIORITY_HIGH } from 'constants'
 
 export async function getServerSideProps(context){
     // await fetch('https://emailrep.io/'+context.params.email,{
@@ -28,7 +29,8 @@ export async function getServerSideProps(context){
       return {
         props:{
             email: context.params.email,
-
+            reputation: 'medium',
+            references: 22 
         }
       }
 }
@@ -38,6 +40,7 @@ export async function getServerSideProps(context){
 
 export default function User(props) {
   const cookies = parseCookies();
+  const img = props.reputation+".jpg"
 
   function handleClick(event){
     event.preventDefault();
@@ -56,16 +59,35 @@ export default function User(props) {
     
     <div>
       <Head>
-        <title>Create Next App</title>
+        <title>🚀🐶 - {props.email}</title>
       </Head>
 
-      <main>
-          {props.email}
-          <button
-            onClick={(e)=> handleClick(e)}
-          >
-            logout
-        </button>
+      <main className='flex h-screen justify-center items-center'>
+        <div className='flex flex-col'>
+          <h1 className='text-5xl text-center'>🚀🐶LaunchBarkly🐶🚀</h1>
+          <h1 className='text-3xl text-center'>
+            {props.email}
+          </h1>
+          <br/>
+            <img 
+              src={img}
+              className="object-contain h-48 w-full" 
+            />
+          <br/>
+          <h1 className='text-2xl text-center'>
+            Your reputation is: <span className="text-purple-700">{props.reputation}</span>
+          </h1>
+          <h2 className='text-2xl text-center '>
+            Email References: <span className="text-purple-700">{props.references}</span>
+          </h2>
+          <br/>
+            <button
+              className="bg-gray-300 hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow"
+              onClick={(e)=> handleClick(e)}
+            >
+              logout
+          </button>
+        </div>
       </main>
 
     </div>
